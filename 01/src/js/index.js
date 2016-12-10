540f78a4c34bedd7ad2acd648f2e06bd5b60e2dd
SiAdcock <simonadcock2@gmail.com>
2016-12-10 19:24:57 +0000

    simple animation

--- 01/src/js/index.js
+++ 01/src/js/index.js
@@ -1,12 +1,29 @@
 (function () {
   'use strict';
 
+  function playAudio(el) {
+    el.currentTime = 0;
+    el.play();
+  }
+
+  function reset() {
+    console.log(this);
+
+    this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+
+  keys.forEach(key => key.addEventListener('transitionend', reset));
   document.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelectorAll(`.key[data-key="${e.keyCode}"]`)[0];
 
     if (audio) {
-      audio.currentTime = 0;
-      audio.play();
+      playAudio(audio);
+    }
+    if (key) {
+      key.classList.add('playing');
     }
   });
 }());
