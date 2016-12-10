39bce6817bdea5f3b6cf4c3a21f61440b713c2ca
SiAdcock <simonadcock2@gmail.com>
2016-12-10 19:15:00 +0000

    play that audio

--- 01/src/js/index.js
+++ 01/src/js/index.js
@@ -1,4 +1,12 @@
-(function() {
+(function () {
   'use strict';
 
+  document.addEventListener('keydown', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+    }
+  });
 }());
